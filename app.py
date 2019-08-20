3acb459dce2fd4dfbd6046009ef1700a0123ae90
Jeremy Chon <jaedee95@gmail.com>
2019-08-20 15:10:32 -0500

    set up app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, g
+from flask_cors import CORS
+from flask_login import LoginManager
+
+
+DEBUG = True
+PORT = 8000
+
+# sets up ability to set up the session
+login_manager = LoginManager
+
+# Initializes an instance of the Flask class (aka starts the website)
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+
+
+@app.before_request
+def before_request():
+	'''Connect to the database'''
+	g.db = models.DATABASE
+	g.db.connect()
+
+@app.after_request
+def after_request():
+	### Close the database after each request ###
+	g.db.close()
+	return response
+
+
+if __name__ == '__main__':
+	models.initialize()
+	app.run(debug=DEBUG, port=PORT)
