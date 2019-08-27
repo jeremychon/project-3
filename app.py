8f26b81f815ab3bc37ac03af0221a96f9b265ee8
Jeremy Chon <jaedee95@gmail.com>
2019-08-26 19:08:11 -0500

    changed DB

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ app.register_blueprint(pp)
 @app.before_request
 def before_request():
 	'''Connect to the database'''
-	g.db = models.Painpoints_API_DB
+	g.db = models.DATABASE
 	g.db.connect()
 
 @app.after_request
